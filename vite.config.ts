7d8ab51c321cd777af075371f4b81bcecc40a655
Lars L <lars@lehmann.link>
2025-01-07 16:38:04 +0100

    .

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	define: {
-		'process.env.NOTION_TOKEN': JSON.stringify(process.env.NOTION_TOKEN),
-		'process.env.NOTION_DATABASE_ID': JSON.stringify(process.env.NOTION_DATABASE_ID),
-	  },
+	plugins: [sveltekit()]
 });
